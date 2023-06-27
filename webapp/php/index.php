84ad9beb434683b6dd3fffbd7273d8f2bfe25c7d
sunamoto-mikiya <miki38gtr@gmail.com>
2023-06-27 16:29:02 +0900

    クエリ最適化

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -105,7 +105,7 @@ $container->set('helper', function ($c) {
         }
 
         public function try_login($account_name, $password) {
-            $user = $this->fetch_first('SELECT * FROM users WHERE account_name = ? AND del_flg = 0', $account_name);
+            $user = $this->fetch_first('SELECT `account_name`,`passhash`, FROM users WHERE account_name = ? AND del_flg = 0', $account_name);
             if ($user !== false && calculate_passhash($user['account_name'], $password) == $user['passhash']) {
                 return $user;
             } elseif ($user) {
