b808e76c94207ab9afa270e60288c5ea22d3f0b8
sunamoto-mikiya <miki38gtr@gmail.com>
2023-06-27 15:01:15 +0900

    不要な演算削除

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -325,7 +325,7 @@ $app->get('/posts', function (Request $request, Response $response) {
     $max_created_at = $params['max_created_at'] ?? null;
     $db = $this->get('db');
     $ps = $db->prepare('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC');
-    $ps->execute([$max_created_at === null ? null : $max_created_at]);
+    $ps->execute([$max_created_at]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
 
